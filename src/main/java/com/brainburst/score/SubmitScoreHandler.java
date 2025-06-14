42f000ac540574566772816734c69003f330562e
AMS-JR <asj.sarjo@gmail.com>
2025-06-14 05:37:10 -0500

    Commit dummy to SubmitScoreHandler

--- src/main/java/com/brainburst/score/SubmitScoreHandler.java
+++ src/main/java/com/brainburst/score/SubmitScoreHandler.java
@@ -26,7 +26,7 @@ public class SubmitScoreHandler implements RequestHandler<APIGatewayProxyRequest
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
             logger.log("Received score submission: " + event.getBody() + "\n");
-            logger.log("AMADOU SARJO JALLOW LOVES RO");
+            logger.log("RO IS SEXY");
             // process event
             logger.log("EVENT: " + gson.toJson(event));
 
